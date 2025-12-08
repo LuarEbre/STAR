291967119654c1d1fd5bde38bc99eb684b1a2a6c
LeleZss <liuzzoleandro@gmail.com>
2025-12-08 17:08:15 +0100

    save commit

--- src/main/java/sumo/sim/SimulationRenderer.java
+++ src/main/java/sumo/sim/SimulationRenderer.java
@@ -15,6 +15,7 @@ import static java.lang.Math.abs;
 public class SimulationRenderer {
     private GraphicsContext gc;
     private Canvas map;
+    private double zoom = 2;
 
     public SimulationRenderer(Canvas canvas, GraphicsContext gc) {
         this.map = canvas;
@@ -37,13 +38,13 @@ public class SimulationRenderer {
         for(JunctionWrap jw : jl.getJunctions()){
             Point2D.Double p = jw.getPosition();
             gc.setFill(Color.RED);
-            gc.fillOval(p.getX()+x_offset, p.getY()+y_offset, 15, 15);
+            gc.fillOval((p.getX()+x_offset)*zoom, (p.getY()+y_offset)*zoom, 15*zoom, 15*zoom);
             jl.print_adjacency();
             for (String s : jl.getAdjacentVertexes(jw.getID())){
                 System.out.println("herawdawkjf"  + s);
                 //gc.strokePolyline();//p.X, p.Y s.getPosition.x, s.getPosition.y -> s id of edges connected to jw
                 gc.setLineWidth(2);
-                gc.strokeLine(p.x+x_offset+7, p.y+y_offset+7, jl.getJunction(s).getPosition().x+x_offset+7, jl.getJunction(s).getPosition().y+y_offset+7);
+                gc.strokeLine((p.x+x_offset+7)*zoom, (p.y+y_offset+7)*zoom, (jl.getJunction(s).getPosition().x+x_offset+7)*zoom, (jl.getJunction(s).getPosition().y+y_offset+7)*zoom);
             }
 
         }
