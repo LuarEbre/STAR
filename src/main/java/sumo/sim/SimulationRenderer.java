b491163db764403e7ff72db937f7289e7c5b5465
LuarEbre <raule0822@gmail.com>
2026-01-05 14:07:33 +0100

    Add TabPane as Statistics Pane

--- src/main/java/sumo/sim/SimulationRenderer.java
+++ src/main/java/sumo/sim/SimulationRenderer.java
@@ -74,7 +74,7 @@ public class SimulationRenderer {
 
         this.showTrafficLightIDs = true;
         this.showRouteHighlighting = true;
-        this.showDensityAnchor = true;
+        this.showDensityAnchor = false;
 
         this.seeTrafficLightIDs = false;
 
