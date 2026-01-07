4439f46cd83ee248ca65b300cf8e32137070aded
s-reese04 <SimonRK@gmx.de>
2026-01-07 18:18:02 +0100

    Fixxed Old Wrong Commit

---
+++ src/main/java/sumo/sim/DrawableObjects.java
@@ -0,0 +1,12 @@
+package sumo.sim;
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * Generic class used for declaring drawable objects and render them
+ */
+public interface DrawableObjects {
+    void draw(GraphicsContext gc, double zoom);
+    double getX();
+    double getY();
+}
