59edfbaba48b97535e1c2836b138bf4344a27d0c
LuarEbre <raule0822@gmail.com>
2026-01-14 22:54:03 +0100

    Close #81

--- src/main/java/sumo/sim/GuiController.java
+++ src/main/java/sumo/sim/GuiController.java
@@ -220,7 +220,8 @@ public class GuiController {
                 addButton,
                 stressTestButton,
                 trafficLightButton,
-                stepButton
+                stepButton,
+                createButton
         };
 
         String[] modes = { "Light Test" , "Medium Test" , "Heavy Test" };
