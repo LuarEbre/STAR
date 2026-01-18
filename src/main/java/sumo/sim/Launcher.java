ed3b50fd0d2d6256a42c7d00df4a3a34d734a674
LeleZss <liuzzoleandro@gmail.com>
2026-01-18 14:24:50 +0100

    Preparing for final launch

---
+++ src/main/java/sumo/sim/Launcher.java
@@ -0,0 +1,11 @@
+package sumo.sim;
+
+public class Launcher {
+    /**
+     * Used for running .exe and preventing javaFX loading issues.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Main.main(args); // calls main class
+    }
+}
